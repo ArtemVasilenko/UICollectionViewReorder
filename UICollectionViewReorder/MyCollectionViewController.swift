0b1841e372a91dd2331c784fe2fd1a413b5ce6af
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-14 21:16:05 +0300

    +

--- UICollectionViewReorder/MyCollectionViewController.swift
+++ UICollectionViewReorder/MyCollectionViewController.swift
@@ -1,16 +1,11 @@
-//
-//  MyCollectionViewController.swift
-//  UICollectionViewReorder
-//
-//  Created by Артем on 6/14/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 private let reuseIdentifier = "Cell"
 
 class MyCollectionViewController: UICollectionViewController {
+    
+    
     
     var charArr: [String] = []
     
@@ -22,68 +17,37 @@ class MyCollectionViewController: UICollectionViewController {
             charArr.append(String($0))
         }
         
+        charArr.shuffle()
+        
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using [segue destinationViewController].
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    // MARK: UICollectionViewDataSource
-    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
     
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return charArr.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! AbcCollectionViewCell
+        cell.abcLabel.text = charArr[indexPath.row]
         
-        // Configure the cell
         
         return cell
     }
     
-    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
     
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        let temp = charArr.remove(at: sourceIndexPath.item)
+        charArr.insert(temp, at: destinationIndexPath.item)
+        
+    }
     
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
     
 }
