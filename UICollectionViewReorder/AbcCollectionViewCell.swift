8c7b115c918c95f303e4e2d53b3870684d3ddbf7
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-14 20:56:29 +0300

    start

---
+++ UICollectionViewReorder/AbcCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AbcCollectionViewCell.swift
+//  UICollectionViewReorder
+//
+//  Created by Артем on 6/14/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class AbcCollectionViewCell: UICollectionViewCell {
+    
+}
